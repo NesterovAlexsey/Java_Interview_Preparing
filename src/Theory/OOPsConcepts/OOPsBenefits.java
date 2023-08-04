b154de990c56cc96773ca86fc88dace4f044927f
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-08-04 12:43:20 +0200

    added OOPs benefits

---
+++ src/Theory/OOPsConcepts/OOPsBenefits.java
@@ -0,0 +1,23 @@
+package Theory.OOPsConcepts;
+
+public class OOPsBenefits {
+    /*
+    Explain the benefits of OOPs
+
+    Answer:
+
+    Following are the benefits of OOPs:
+
+        Reusability: OOPs principles like Inheritance, Composition
+and polymorphism help in reusing existing code
+        Extensibility: Code written using OOPs principles like
+Inheritance makes code extensible
+        Security: OOPs principles like encapsulation help to keep
+the data and the code that operates on the data together and
+makes the code secure
+        Simplicity: Java classes represent real world objects. This
+makes code very easy to understand
+        Maintainability: Code written using OOPs principles is
+easier to maintain
+     */
+}
