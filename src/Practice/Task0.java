5b0640dddf8a8e7da2dee39526784706e1717e15
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-09-18 09:34:19 +0200

    Task FizzBuss have been done

---
+++ src/Practice/Task0.java
@@ -0,0 +1,37 @@
+package Practice;
+
+/*
+Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+ The first word within the output should be capitalized only if the original word was capitalized
+ (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
+
+Examples
+"the-stealth-warrior" gets converted to "theStealthWarrior"
+"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
+ */
+
+public class Task0 {
+    public static void main(String[] args) {
+        String s = "the-stealth-warrior";
+        String s2 = "The_Stealth_Warrior";
+        String s3 = "The_Stealth-Warrior";
+
+        System.out.println(toCamelCase(s));
+        System.out.println(toCamelCase(s2));
+        System.out.println(toCamelCase(s3));
+
+    }
+
+    static String toCamelCase(String s) {
+        String[] words = s.split("-");
+        String result = "";
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                char[] bigLetter = words[i].toCharArray();
+            }
+            result = result + words[i];
+        }
+        return result;
+    }
+}
--- src/Practice/Task1.java
+++ src/Practice/Task1.java
@@ -1,40 +1,22 @@
 package Practice;
+//FizzBuzz: Write a program that prints numbers from 1 to 100, but prints "Fizz" for numbers that are multiples of 3,
+// "Buzz" for numbers that are multiples of 5, and "FizzBuzz" for numbers that are multiples of both 3 and 5.
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
-/*
-Complete the method/function so that it converts dash/underscore delimited words into camel casing.
- The first word within the output should be capitalized only if the original word was capitalized
- (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.
-
-Examples
-"the-stealth-warrior" gets converted to "theStealthWarrior"
-"The_Stealth_Warrior" gets converted to "TheStealthWarrior"
-"The_Stealth-Warrior" gets converted to "TheStealthWarrior"
- */
+import java.util.List;
 
 public class Task1 {
-    public static void main(String[] args) {
-        String s = "the-stealth-warrior";
-        String s2 = "The_Stealth_Warrior";
-        String s3 = "The_Stealth-Warrior";
-
-        System.out.println(toCamelCase(s));
-        System.out.println(toCamelCase(s2));
-        System.out.println(toCamelCase(s3));
-
+  public static void main(String[] args) {
+    Integer num;
+    for (num = 1; num <= 100; num++) {
+      if (num % 15 == 0) {
+        System.out.println("FizzBuzz");
+      } else if (num % 5 == 0) {
+        System.out.println("Buzz");
+      } else if (num % 3 == 0) {
+        System.out.println("Fizz");
+      } else {
+        System.out.println(num);
+      }
     }
-
-    static String toCamelCase(String s) {
-        String[] words = s.split("-");
-        String result = "";
-        for (int i = 0; i < words.length; i++) {
-            if (i > 0) {
-                char[] bigLetter = words[i].toCharArray();
-            }
-            result = result + words[i];
-        }
-        return result;
-    }
+  }
 }
