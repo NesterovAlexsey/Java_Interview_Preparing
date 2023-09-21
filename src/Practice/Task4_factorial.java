e20b80a13940c11df51f27ead9eba52c3f86b74d
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-09-21 14:59:02 +0200

    start to work with task about factorial

---
+++ src/Practice/Task4_factorial.java
@@ -0,0 +1,10 @@
+package Practice;
+//Write a function to calculate the factorial of a number.
+public class Task4_factorial {
+
+  public static Integer factorial(Integer number) {
+
+    Integer result = 0;
+    return result;
+  }
+}
