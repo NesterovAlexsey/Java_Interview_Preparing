12343481e4d9f15a57fd2bcb9145eec3f814ce88
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-09-25 06:42:08 +0200

    initial commit for task 5

--- src/Practice/Task5_reverseString/Task5_reverseString.java
+++ src/Practice/Task5_reverseString/Task5_reverseString.java
@@ -1,4 +1,9 @@
 package Practice.Task5_reverseString;
 
+//Task: Write a program to reverse a string
 public class Task5_reverseString {
+  public String reverseString(String text){
+    String result = "";
+    return result;
+  }
 }
