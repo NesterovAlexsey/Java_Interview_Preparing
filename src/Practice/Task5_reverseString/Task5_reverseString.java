f6a6b39fa2073ad25532e77cb7f5704574333907
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-09-26 07:21:41 +0200

    resolve task 5

--- src/Practice/Task5_reverseString/Task5_reverseString.java
+++ src/Practice/Task5_reverseString/Task5_reverseString.java
@@ -1,9 +1,51 @@
 package Practice.Task5_reverseString;
 
+import java.util.ArrayList;
+
 //Task: Write a program to reverse a string
 public class Task5_reverseString {
-  public String reverseString(String text){
-    String result = "";
+
+  public static void main (String[] args) {
+    //todo unit tests
+    System.out.println(reverseStringWithLibrary("123456"));
+    System.out.println(reverseStringWithCharArray("1234567"));
+    System.out.println(reverseString("12345678"));
+  }
+
+  public static String reverseStringWithCharArray(String text){
+    //todo if it is not empty
+
+    char[] forReverse = text.toCharArray();
+
+    int left = 0;
+    int right = forReverse.length - 1;
+
+    while(right >= left) {
+
+      char temp = forReverse[left];
+      forReverse[left] = forReverse[right];
+      forReverse[right] = temp;
+
+      left++;
+      right--;
+    }
+    String result = new String(forReverse);
+
     return result;
   }
+
+  public static String reverseStringWithLibrary(String text) {
+    return new StringBuilder(text).reverse().toString();
+  }
+
+  public static String reverseString(String text) {
+    String reverse = "";
+
+    for (int i = text.length() - 1; i >= 0; i--) {
+      reverse = reverse + text.charAt(i);
+    }
+
+    return reverse;
+  }
+
 }
