8840e4009674e43c08df0a3d08e88bcc3b2f32e4
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-10-05 16:56:19 +0200

    start to investigate Docker theory

---
+++ src/TheoryForProjects/Docker/DockerTheory.java
@@ -0,0 +1,21 @@
+package TheoryForProjects.Docker;
+
+public class DockerTheory {
+
+}
+
+/**
+ * Docker is a platform for developing, shipping, and running applications in containers. It uses containerization
+ * technology to allow applications to be packaged with their dependencies into a standardized unit, making it easy
+ * to deploy and run them on any environment that supports Docker. Containers are lightweight, portable, and efficient,
+ * enabling developers to build applications that can run consistently across various environments.
+ *
+ * based on linux
+ *
+ * docker pull postgres - download docker version for postgres
+ *
+ * Video:
+ * Part 1 - https://youtu.be/_JsBufvF_m8
+ * Part 2 - https://youtu.be/nrw6YsChQxE
+ * Part 3 - https://youtu.be/vRWVFJNzl00
+ */
