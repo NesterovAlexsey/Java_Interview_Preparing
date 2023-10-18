3624b1cb740c52cdeae8aca072c6b618c64663fe
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-10-18 07:03:46 +0200

    code from book

---
+++ src/SierraBookExersize/Compilation.java
@@ -0,0 +1,14 @@
+package SierraBookExersize;
+
+public class Compilation {
+
+  public static void main(String [] args) {
+    int x = 5;
+    while (x > 1) {
+      x = x - 1;
+      if (x < 3) {
+        System.out.println("small x");
+      }
+    }
+  }
+}
