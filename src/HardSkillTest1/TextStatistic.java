6a53daca4bd43770052279b40e810cc31435d75b
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-11-04 07:45:51 +0100

    finish with task TextStatistic

--- src/HardSkillTest1/TextStatistic.java
+++ src/HardSkillTest1/TextStatistic.java
@@ -8,7 +8,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class TextStatistic {
 
@@ -16,7 +18,9 @@ public class TextStatistic {
   public static void main(String[] args) {
 
     List<String> story = getText();
-    System.out.println(getNumberOfWords(story));
+    System.out.println("Number of words: " + getNumberOfWords(story));
+    System.out.println("Number of sentences: " + getNumberOfSentences(story));
+    System.out.println("Number of unic words: " + getNumberOfUniqueWords(story));
 
   }
 
@@ -31,9 +35,11 @@ public class TextStatistic {
       }
 
       reader.close();
+
     } catch (IOException exception){
       exception.printStackTrace();
     }
+
     return story;
   }
 
@@ -42,9 +48,36 @@ public class TextStatistic {
 
     for (String line : story) {
       String[] words = line.split("\\s+");
-      result = result + words.length;
+      result += words.length;
     }
 
     return result;
+  }
+
+  private static int getNumberOfSentences(List<String> story) {
+    int result = 0;
+
+    for (String line : story) {
+      String[] sentences = line.split("[.!?\\.{3}]");
+      //todo should to avoid abbreviation
+      result += sentences.length;
+    }
+    return result;
+  }
+
+  private static int getNumberOfUniqueWords(List<String> story) {
+    Set<String> unique = new HashSet<>();
+
+    for (String line : story) {
+      String[] words = line.split("\\s");
+      for (String word : words) {
+        String realWord = word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+        if (!realWord.isEmpty()) {
+          unique.add(realWord);
+        }
+      }
+    }
+
+    return unique.size();
   }
 }
